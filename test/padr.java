0d189b5d15ab3d40300e17c6acac819617127cbd
Perhaps12 <139695749+Perhaps12@users.noreply.github.com>
2025-04-24 21:51:06 -0400

    Delete test directory

--- test/padr.java
+++
@@ -1,9 +0,0 @@
-public class padr {
-    public double first;
-    public double second;
-
-    public void set (double x, double y){
-        first = x;
-        second = y;
-    }
-}
--- test/walls.java
+++
@@ -1,7 +0,0 @@
-
-public class walls{
-    public final static int numWall = 30;
-    public static pair[][] bounds = new pair[numWall][2];
-    public static boolean[] active = new boolean[numWall];
-
-}
