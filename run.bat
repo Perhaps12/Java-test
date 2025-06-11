1fd3d5baeb6731c4a58249830eabfacc27acc821
Kevin <72520741+kevinyhe@users.noreply.github.com>
2025-06-11 13:47:58 -0400

    file management

--- run.bat
+++ run.bat
@@ -1,11 +1,21 @@
 @echo off
 echo Compiling Java files..
-javac *.java
+javac *.java -d bin
 if errorlevel 1 (
     echo Compilation failed!
     pause
     exit /b 1
 )
+
+echo Copying assets to bin directory...
+if not exist "bin\Sprites" mkdir "bin\Sprites"
+if not exist "bin\textures" mkdir "bin\textures"
+if not exist "bin\Static" mkdir "bin\Static"
+
+xcopy /E /Y "Sprites\*" "bin\Sprites\" >nul 2>&1
+xcopy /E /Y "textures\*" "bin\textures\" >nul 2>&1
+xcopy /E /Y "Static\*" "bin\Static\" >nul 2>&1
+
 echo Compilation successful! Running game...
-java Main
+java -cp bin Main
 pause
